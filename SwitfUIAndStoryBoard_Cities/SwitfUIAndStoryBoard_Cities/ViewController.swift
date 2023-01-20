7cd36d732bfcc9a31715482048610fd977812a28
RaniaArbash <41278913+RaniaArbash@users.noreply.github.com>
2023-01-20 12:57:32 -0500

    Add files via upload

---
+++ SwitfUIAndStoryBoard_Cities/SwitfUIAndStoryBoard_Cities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwitfUIAndStoryBoard_Cities
+//
+//  Created by Rania Arbash on 2023-01-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
